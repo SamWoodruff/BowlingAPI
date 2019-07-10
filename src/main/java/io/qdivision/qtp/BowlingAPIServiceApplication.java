b4e3d4acc9d501811c7da08997c2fa09f8697e05
tsmw2 <tsmw2@unigroupinc.com>
2019-07-10 17:54:59 -0500

    Spring Setup

---
+++ src/main/java/io/qdivision/qtp/BowlingAPIServiceApplication.java
@@ -0,0 +1,13 @@
+package io.qdivision.qtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingAPIServiceApplication {
+    public static void main(String[] args){
+        SpringApplication.run(BowlingAPIServiceApplication.class, args);
+    }
+
+}
+
